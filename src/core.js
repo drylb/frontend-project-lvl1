be7d7ae0de56e8ca814a0fce7b69563f3b6199f7
vladsergy <drylb924@gmail.com>
2021-01-09 00:27:06 +0200

    fixing typos 2

--- src/core.js
+++ src/core.js
@@ -15,7 +15,7 @@ const playGame = (gameDescription, gameData) => {
       console.log('Correct!');
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
-      console.log(`Let's try again ${userName}`);
+      console.log(`Let's try again ${userName}!`);
       return;
     }
   }
