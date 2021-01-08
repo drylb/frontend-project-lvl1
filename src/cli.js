da7c7c491514ebf34b9a67333f6bd21276473be4
vladsergy <drylb924@gmail.com>
2021-01-08 21:36:08 +0200

    refactoring

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,8 @@ import readlineSync from 'readline-sync';
 
 const askUsernName = () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name ?');
-  console.log(`Hi ${userName} !`);
+  const userName = readlineSync.question('May I have your name ? ');
+  console.log(`Hello ${userName} !`);
 };
 
 export default askUsernName;
