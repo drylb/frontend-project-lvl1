e7d2012fbdd8fe8bd2578fa40d0d02b815422b3e
vladsergy <drylb924@gmail.com>
2021-01-08 22:50:33 +0200

    refactoring 3

--- src/core.js
+++ src/core.js
@@ -4,9 +4,9 @@ const numberOfRounds = 3;
 
 const playGame = (gameDescription, gameData) => {
   console.log('Welcome to the brain games');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
   console.log(gameDescription);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   for (let i = 0; i < numberOfRounds; i += 1) {
     const { question, answer } = gameData();
     console.log(`Qestion: ${question}`);
@@ -19,10 +19,11 @@ const playGame = (gameDescription, gameData) => {
       return;
     }
   }
-  console.log(`Congratulations ${userName}!`);
+  console.log(`Congratulations, ${userName}!`);
 };
 
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
 export {
   playGame,
   getRandom,
